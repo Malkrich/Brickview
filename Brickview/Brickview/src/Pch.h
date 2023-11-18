ed6a3a7940ab00eefa3dad91c2ee36700de9c5a8
Malkrich <mathurin.lemarie@gmail.com>
2023-11-18 18:16:58 +0100

    adding Pch.h/.cpp

---
+++ Brickview/Brickview/src/Pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// basics
+#include <string>
+#include <fstream>
+#include <memory>
+#include <functional>
+#include <cmath>
+
+// containers
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <map>
