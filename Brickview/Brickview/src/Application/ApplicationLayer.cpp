ff7844cfd24046842e135f6f95fe8e0d15bcb8b6
Malkrich <mathurin.lemarie@gmail.com>
2023-11-20 17:14:43 +0100

    adding applicationLayer

---
+++ Brickview/Brickview/src/Application/ApplicationLayer.cpp
@@ -0,0 +1,66 @@
+#include "Pch.h"
+#include "ApplicationLayer.h"
+
+#include "Core/Core.h"
+#include "Core/Log.h"
+
+#include <glad/glad.h>
+
+namespace Brickview
+{
+	ApplicationLayer::~ApplicationLayer()
+	{
+	}
+
+	void ApplicationLayer::onAttach()
+	{
+		m_colorShader.reset(new Shader("data/color.vs", "data/color.fs"));
+
+		// Vertex buffer and index buffer
+		float positions[] = { -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+							   0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+							   0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+							  -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f,
+							   1.0f,  0.0f, 0.0f, 1.0f, 1.0f, 0.0f };
+
+		unsigned int indices[] = { 0, 1, 2,
+								   2, 3, 0,
+								   1, 4, 2 };
+
+		m_vertexArray.reset(new VertexArray());
+
+		m_vertexBuffer.reset(new VertexBuffer(sizeof(positions), positions));
+		m_vertexArray->addVertexBuffer(m_vertexBuffer);
+
+		m_indexBuffer.reset(new IndexBuffer(sizeof(indices), indices));
+		m_vertexArray->setIndexBuffer(m_indexBuffer);
+	}
+	
+	void ApplicationLayer::onDetach()
+	{
+
+	}
+
+	void ApplicationLayer::onEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+
+		dispatcher.dispatch<MouseMoveEvent>(BV_BIND_EVENT_FUNCTION(ApplicationLayer::onMouseMove));
+	}
+
+	void ApplicationLayer::onUpdate(float dt)
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+		m_colorShader->bind();
+		m_vertexArray->bind();
+		glDrawElements(GL_TRIANGLES, m_vertexArray->getIndexBuffer()->getCount(), GL_UNSIGNED_INT, 0);
+	}
+
+	bool ApplicationLayer::onMouseMove(const MouseMoveEvent& e)
+	{
+		BV_LOG_INFO("Mouse move event : {0},{1}", e.getPosX(), e.getPosY());
+		return true;
+	}
+
+}
---
+++ Brickview/Brickview/src/Application/ApplicationLayer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Core/Layer/Layer.h"
+#include "Core/Event/Event.h"
+#include "Renderer/Buffer/Buffer.h"
+#include "Renderer/Shader/Shader.h"
+
+namespace Brickview
+{
+
+	class ApplicationLayer : public Layer
+	{
+	public:
+		ApplicationLayer() = default;
+		virtual ~ApplicationLayer();
+
+		virtual void onAttach() override;
+		virtual void onDetach() override;
+
+		virtual void onEvent(Event& e) override;
+		virtual void onUpdate(float dt) override;
+
+	private:
+		bool onMouseMove(const MouseMoveEvent& e);
+
+	private:
+		std::shared_ptr<VertexArray> m_vertexArray;
+		std::shared_ptr<VertexBuffer> m_vertexBuffer;
+		std::shared_ptr<IndexBuffer> m_indexBuffer;
+		std::shared_ptr<Shader> m_colorShader;
+	};
+
+}
