e7dd721fa4e8c90c77e057435c6930474820f324
Malkrich <mathurin.lemarie@gmail.com>
2024-02-22 16:25:21 +0100

    moving assert to Core.h

--- Brickview/Brickview/src/Core/Core.h
+++ Brickview/Brickview/src/Core/Core.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include "Log.h"
+
 #define BV_BIND_EVENT_FUNCTION(function) std::bind(&function, this, std::placeholders::_1)
+
+#ifdef BV_DEBUG
+	#define BV_ASSERT(condition, ...) if(!condition) { BV_LOG_ERROR(__VA_ARGS__); }
+#else
+	#define BV_ASSERT(condition, ...)
+#endif
--- Brickview/Brickview/src/Core/Log.h
+++ Brickview/Brickview/src/Core/Log.h
@@ -25,13 +25,9 @@ namespace Brickview
 	#define BV_LOG_INFO(...)	Brickview::Log::getLogger()->info(__VA_ARGS__)
 	#define BV_LOG_WARN(...)	Brickview::Log::getLogger()->warn(__VA_ARGS__)
 	#define BV_LOG_ERROR(...)	Brickview::Log::getLogger()->error(__VA_ARGS__)
-
-	#define BV_ASSERT(condition, ...) if(!condition) { BV_LOG_ERROR(__VA_ARGS__); }
 #else
 	#define BV_LOG_TRACE(...)
 	#define BV_LOG_INFO(...)
 	#define BV_LOG_WARN(...)
 	#define BV_LOG_ERROR(...)
-
-	#define BV_ASSERT(condition, ...)
 #endif
