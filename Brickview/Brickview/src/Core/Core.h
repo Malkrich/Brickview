1553c56a2a26353b377972d302c3977735ba57ce
Malkrich <mathurin.lemarie@gmail.com>
2024-04-11 18:17:57 +0200

    added debugbreak call when assert

--- Brickview/Brickview/src/Core/Core.h
+++ Brickview/Brickview/src/Core/Core.h
@@ -5,7 +5,7 @@
 #define BV_BIND_EVENT_FUNCTION(function) std::bind(&function, this, std::placeholders::_1)
 
 #ifdef BV_DEBUG
-	#define BV_ASSERT(condition, ...) if(!condition) { BV_LOG_ERROR(__VA_ARGS__); }
+	#define BV_ASSERT(condition, ...) if(!(condition)) { BV_LOG_ERROR(__VA_ARGS__); __debugbreak(); }
 #else
 	#define BV_ASSERT(condition, ...)
 #endif
