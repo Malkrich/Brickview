935a1faa36b5d8da3a105b545d6cde406d515c30
Malkrich <mathurin.lemarie@gmail.com>
2024-10-25 16:49:43 +0200

    removed entity list

--- BrickviewApp/src/Panels/ScenePartsListPanel.cpp
+++ BrickviewApp/src/Panels/ScenePartsListPanel.cpp
@@ -21,20 +21,6 @@ namespace Brickview
 			ImGui::Text("%s", id.c_str());
 		}
 
-		// TEMP
-		ImGui::SeparatorText("Entities");
-
-		auto legoInstances = m_sceneContext->m_registry.view<TransformComponent, LegoPartComponent>();
-		for (auto e : legoInstances)
-		{
-			Entity entity = { e, m_sceneContext.get() };
-			LegoPartID partID = entity.getComponent<LegoPartComponent>().ID;
-			glm::vec3& translation = entity.getComponent<TransformComponent>().Translation;
-
-			std::string posName = std::format("{}##{}", partID, (uint32_t)entity);
-			ImGui::DragFloat3(posName.c_str(), glm::value_ptr(translation), 0.005f);
-		}
-
 		ImGui::End();
 	}
 
