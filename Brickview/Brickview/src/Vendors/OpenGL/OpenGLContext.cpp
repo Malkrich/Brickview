795e3b22cf9ac5c9901885ee2352894223931617
Malkrich <mathurin.lemarie@gmail.com>
2025-03-03 13:33:03 +0100

    changed opengl context logs

--- Brickview/Brickview/src/Vendors/OpenGL/OpenGLContext.cpp
+++ Brickview/Brickview/src/Vendors/OpenGL/OpenGLContext.cpp
@@ -24,12 +24,11 @@ namespace Brickview
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		BV_ASSERT(status, "Failed to initialize Glad!");
 
-		BV_LOG_INFO("OpenGL Info:");
-		BV_LOG_INFO("  - Vendor: {0}", (char*)glGetString(GL_VENDOR));
-		BV_LOG_INFO("  - Renderer: {0}", (char*)glGetString(GL_RENDERER));
-		BV_LOG_INFO("  - Version: {0}", (char*)glGetString(GL_VERSION));
+		BV_LOG_INFO("Using OpenGL API version {}", (char*)glGetString(GL_VERSION));
+		BV_LOG_INFO(" - Vendor: {}", (char*)glGetString(GL_VENDOR));
+		BV_LOG_INFO(" - Device: {}", (char*)glGetString(GL_RENDERER));
 
-		BV_ASSERT(GLVersion.major > 4 || (GLVersion.major == 4 && GLVersion.minor >= 5), "Hazel requires at least OpenGL version 4.5!");
+		BV_ASSERT(GLVersion.major > 4 || (GLVersion.major == 4 && GLVersion.minor >= 5), "Brickview requires at least OpenGL version 4.5!");
 	}
 
 	void OpenGLContext::swapBuffers()
